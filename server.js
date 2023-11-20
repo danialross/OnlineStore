d590e7d835a32ef7ab4a3e1716ce940b4dbf091f
danialross <92746365+danialross@users.noreply.github.com>
2023-11-19 23:14:59 -0500

    added jwt
    added json web token and give to users when logged in successfully, used token for adding to carts and refactored

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 3000;
 const saltRounds = 10;
 const users = [];
 app.use(express.json());
+const secretKey = "super-secret-key";
 
 const initializeDB = async () => {
   const accounts = [
@@ -13,7 +15,7 @@ const initializeDB = async () => {
       username: "customer1",
       password: "qwerty",
       cart: [
-        { id: 9, quantity: 1 },
+        { id: 9, quantity: 2 },
         { id: 10, quantity: 1 },
         { id: 11, quantity: 1 },
       ],
@@ -38,11 +40,10 @@ const initializeDB = async () => {
   ];
   for (const account of accounts) {
     try {
-      const hashedUsername = await bcrypt.hash(account.username, saltRounds);
       const hashedPassword = await bcrypt.hash(account.password, saltRounds);
 
       const user = {
-        username: hashedUsername,
+        username: account.username,
         password: hashedPassword,
         cart: account.cart,
       };
@@ -89,7 +90,7 @@ app.get("/items/:id", async (req, res) => {
 });
 
 // check credentials
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -100,49 +101,88 @@ app.post("/login", (req, res) => {
     return res.status(200).json({ data: "" });
   }
 
-  // Retrieve hashed password from your database
-  bcrypt.compare(password, user.password, (err, result) => {
-    if (err) {
-      return res.status(500).json({ err: "Error while comparing passwords:" });
+  // Retrieve hashed password from database
+  try {
+    const result = await bcrypt.compare(password, user.password);
+    if (result) {
+      const token = jwt.sign({ username: username }, secretKey, {
+        expiresIn: "1h",
+      });
+
+      // Send the token in the response
+      res.status(200).json({ data: { user, token } });
     } else {
-      if (result) {
-        res.status(200).json({ data: user });
-      } else {
-        res.status(200).json({ data: "" });
-      }
+      res.status(200).json({ data: "" });
     }
-  });
+  } catch {
+    return res.status(500).json({ err: "Error while comparing passwords" });
+  }
 });
 
+const authenticateToken = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ error: "Access denied" });
+  }
+
+  jwt.verify(token.split(" ")[1], secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
 // add to cart
-app.put("/addToCart", (req, res) => {
-  //requires username to be hashed first
-  //   const itemId = req.body.itemId;
-  //   const username = req.body.username;
-  //   const amount = req.body.quantity;
-  //   const user = users.find((user) => {
-  //     user.username === username;
-  //   });
-  //   const itemInCart = user.cart.find((item) => {
-  //     item.id === itemId;
-  //   });
-  //   if (amount > 0) {
-  //     if (itemInCart) {
-  //       itemInCart.quantity = amount;
-  //     } else {
-  //       const cartItem = { id: itemId, quantity: amount };
-  //       user.cart.push(cartItem);
-  //     }
-  //   } else {
-  //     const arrayWithoutItem = user.cart.filter((itemId) => {
-  //       itemId !== id;
-  //     });
-  //     user.cart = arrayWithoutItem;
-  //   }
-  //   // user will exist because if not it will propt user to login or register
-  //   res
-  //     .send(200)
-  //     .json("item id: " + itemId + "added. user card now : " + userCart);
+app.put("/addToCart", authenticateToken, async (req, res) => {
+  const itemId = req.body.itemId;
+  const amount = req.body.quantity;
+  const username = req.body.username;
+
+  console.log(itemId);
+  console.log(amount);
+  console.log(username);
+
+  const user = users.find((user) => {
+    return user.username === username;
+  });
+  console.log(user);
+
+  const itemInCart = user.cart.find((item) => {
+    return item.id === itemId;
+  });
+
+  console.log(itemInCart);
+
+  if (amount > 0) {
+    if (itemInCart) {
+      itemInCart.quantity = amount;
+    } else {
+      const cartItem = { id: itemId, quantity: amount };
+      user.cart.push(cartItem);
+      console.log("pushed");
+    }
+  } else {
+    const arrayWithoutItem = user.cart.filter((item) => {
+      return item.id !== itemId;
+    });
+    user.cart = arrayWithoutItem;
+  }
+
+  // user will exist because if not it will propt user to login or register
+  const idInCart = [];
+  for (const object of user.cart) {
+    for (let i = 0; i < object.quantity; i++) {
+      idInCart.push(object.id);
+    }
+  }
+
+  res
+    .status(200)
+    .json("item id: " + itemId + " added. user cart now : " + idInCart);
 });
 
 //sign up
@@ -150,11 +190,10 @@ app.post("/register", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   try {
-    const hashedUsername = await bcrypt.hash(username, saltRounds);
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     const user = {
-      username: hashedUsername,
+      username: username,
       password: hashedPassword,
       cart: [],
     };
@@ -162,7 +201,7 @@ app.post("/register", async (req, res) => {
     console.log(users);
     res.status(200).json({ message: username + " created" });
   } catch {
-    console.error("Error while hashing password");
+    console.error("Error while creating user");
     res.status(500);
   }
 });
