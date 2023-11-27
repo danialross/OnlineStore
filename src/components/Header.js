c4741f39001b1d23d1e6e10757a018e555ba2792
danialross <92746365+danialross@users.noreply.github.com>
2023-11-27 00:18:58 -0500

    refactored
    using react bootstrap and styled components instead of tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 function Header() {
-  return;
+  const [username, setUsername] = useState("");
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Button variant="secondary">
+            {username === "" ? "Login" : username}
+          </Button>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
