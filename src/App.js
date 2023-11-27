c8be2821621506fd7b82e4e61a29af0db38214c4
danialross <92746365+danialross@users.noreply.github.com>
2023-11-27 17:31:24 -0500

    added browser route from react
    added routes and navlinks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={
+            <Header>
+              <HomePage />
+            </Header>
+          }
+        />
+        <Route
+          path="/sale"
+          element={
+            <Header>
+              <h1>empty</h1>
+            </Header>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-function Header() {
-  const [username, setUsername] = useState("");
+function Header(props) {
+  const [username, setUsername] = useState("Kevin");
 
   const StyledNavbar = styled(Navbar)`
     background-color: #89abe3;
@@ -17,16 +17,21 @@ function Header() {
 
   const StyledButton = styled(Button)`
     background-color: white;
-    color: #89abe3ff;
+    color: #89abe3;
     border-color: white;
 
     &:hover {
       background-color: #edcac4;
       border-color: #edcac4;
     }
+
+    &:active {
+      background-color: #edcac4;
+    }
   `;
 
-  const StyledBrand = styled(Navbar.Brand)`
+  const StyledBrand = styled(NavLink)`
+    text-decoration: none;
     font-size: 2rem;
     color: white;
 
@@ -37,13 +42,37 @@ function Header() {
     }
   `;
 
-  const StyledLink = styled(Nav.Link)`
+  const StyledLink = styled(NavLink)`
+    text-decoration: none;
     font-size: 1.3rem;
     color: white;
-    margin-right: 1rem;
+    margin-right: 2rem;
 
     &:hover {
       color: #edcac4;
+    }
+
+    &.active {
+      color: #edcac4;
+    }
+  `;
+
+  const StyledDropDown = styled(Dropdown.Toggle)`
+    background-color: white;
+    color: #89abe3;
+    border-radius: 5px;
+    border-color: white;
+
+    &:hover {
+      color: white;
+      background-color: #edcac4;
+      border-color: #edcac4;
+    }
+
+    &:active {
+      color: white;
+      background-color: #edcac4;
+      border-color: #edcac4;
     }
   `;
 
@@ -51,30 +80,33 @@ function Header() {
     <>
       <StyledNavbar>
         <Container>
-          <StyledBrand href="#home">Online Store</StyledBrand>
+          <StyledBrand to="/">Online Store</StyledBrand>
 
           <Nav className="me-auto">
-            <StyledLink href="#home">Home</StyledLink>
-            <StyledLink href="#features">Sale</StyledLink>
+            <StyledLink to="/forhim">Gifts for Him</StyledLink>
+            <StyledLink to="/forher">Gifts for Her</StyledLink>
+            <StyledLink to="/under10">Gifts for under $10</StyledLink>
           </Nav>
 
           {username === "" ? (
-            <StyledButton>Login / Register</StyledButton>
+            <StyledButton variant="secondary">Login / Register</StyledButton>
           ) : (
             <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <StyledDropDown id="dropdown-basic" variant="secondary">
                 {username}
-              </Dropdown.Toggle>
+              </StyledDropDown>
 
               <Dropdown.Menu>
                 <Dropdown.Item href="#/action-1">Cart</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Log Out</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-3">Logout</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           )}
         </Container>
       </StyledNavbar>
+      {props.children}
     </>
   );
 }
