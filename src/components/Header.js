17c970f6aa3ce3767943f64c44179c5027d41e9f
danialross <92746365+danialross@users.noreply.github.com>
2023-11-27 13:03:00 -0500

    navbar design
    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,80 @@
 import { useState } from "react";
 import styled from "styled-components";
-
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [username, setUsername] = useState("");
+
+  const StyledNavbar = styled(Navbar)`
+    background-color: #89abe3;
+    height: 5rem;
+  `;
+
+  const StyledButton = styled(Button)`
+    background-color: white;
+    color: #89abe3ff;
+    border-color: white;
+
+    &:hover {
+      background-color: #edcac4;
+      border-color: #edcac4;
+    }
+  `;
+
+  const StyledBrand = styled(Navbar.Brand)`
+    font-size: 2rem;
+    color: white;
+
+    margin-right: 5rem;
+
+    &:hover {
+      color: #edcac4;
+    }
+  `;
+
+  const StyledLink = styled(Nav.Link)`
+    font-size: 1.3rem;
+    color: white;
+    margin-right: 1rem;
+
+    &:hover {
+      color: #edcac4;
+    }
+  `;
+
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <StyledNavbar>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <StyledBrand href="#home">Online Store</StyledBrand>
+
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <StyledLink href="#home">Home</StyledLink>
+            <StyledLink href="#features">Sale</StyledLink>
           </Nav>
-          <Button variant="secondary">
-            {username === "" ? "Login" : username}
-          </Button>
+
+          {username === "" ? (
+            <StyledButton>Login / Register</StyledButton>
+          ) : (
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                {username}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Cart</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Log Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
         </Container>
-      </Navbar>
+      </StyledNavbar>
     </>
   );
 }
