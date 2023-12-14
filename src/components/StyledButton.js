06f82b0c7c2168bdbc01bdf5477105c12e17c0b5
danialross <92746365+danialross@users.noreply.github.com>
2023-12-13 23:39:04 -0500

    button icons
    styled button now can hold text and icon or just icons

--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CustomButton = styled(Button)`
   background-color: white;
@@ -16,10 +17,39 @@ const CustomButton = styled(Button)`
   }
 `;
 
-function StyledButton(prop) {
+const StyledDiv = styled.div`
+  display: flex;
+  margin-right: 10px;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const IconDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+`;
+
+function StyledButton({ variant, onClick, icon, text }) {
   return (
-    <CustomButton variant={prop.variant} onClick={prop.onClick}>
-      {prop.text}
+    <CustomButton variant={variant} onClick={onClick}>
+      <Row>
+        {/* has text and icon */}
+        {text && icon && <StyledDiv>{text}</StyledDiv>}
+
+        {/* has text but no icon */}
+        {text && !icon && text}
+
+        {/* has icon */}
+        {icon && (
+          <IconDiv>
+            <FontAwesomeIcon icon={icon} />
+          </IconDiv>
+        )}
+      </Row>
     </CustomButton>
   );
 }
